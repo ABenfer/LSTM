0b3ed1db239d3cc2ee44c90f6e946f95c3135ee4
achim <achim.benfer@outlook.com#>
2023-05-15 14:48:42 +0200

    cleaned up

--- config.py
+++ config.py
@@ -1,25 +1,21 @@
 print("ver config = 0.5.9")
 
-# Data import
-DATA_PATH = '/content/'
-SIMS_PATH = DATA_PATH + 'sims_interp/'
-
+# for local use
 # SIMS_PATH = 'sims_interp/'
 
-REAL_FEATURES_PATH = DATA_PATH + 'real_features/'
-TIME_SERIES_PATH = DATA_PATH + 'time_series/'
-TIME_SERIES_INTERP_PATH = DATA_PATH + 'time_series_interp/'
+# Kaggle
+# SIMS_PATH = '/kaggle/input/sims-interp-5000/sims_interp/'
+# CHECKPOINT_PATH = '/kaggle/working/Data/checkpoints/'
+# PLOT_PATH = '/kaggle/working/Data/plots/'
 
-SCRIPT_PATH = "/content/GDrive/MyDrive/Masterarbeit/"
-CHECKPOINT_PATH = SCRIPT_PATH + "Data/checkpoints/"
-PLOT_PATH = SCRIPT_PATH + "Data/plots/"
-
-N_DATA_POINTS = -1
-# N_SAMPLES = 1000
+# Colab
+SIMS_PATH = '/content/sims_interp/'
+CHECKPOINT_PATH = '/content/GDrive/MyDrive/Masterarbeit/Data/checkpoints/'
+PLOT_PATH = '/content/GDrive/MyDrive/Masterarbeit/Data/plots/'
 
 # Model
-LEARNING_RATE = 0.005
-LEARNING_RATE_DECAY = 0.995
+LEARNING_RATE = 0.01
+LEARNING_RATE_DECAY = 0.99
 BATCH_SIZE = 64
 EPOCHS = 400
-EARLY_STOPPING = 50
+EARLY_STOPPING = 40
